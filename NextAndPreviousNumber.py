756dc3053504dbd832f6c827622d7ea782d10abe
spaceindependence <spaceindependence@protonmail.com>
2017-10-23 19:31:30 +0300

    NextAndPreviousNumber.py

--- NextAndPreviousNumber.py
+++ NextAndPreviousNumber.py
@@ -1,3 +1,4 @@
+#Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!).
 number = int(input())
 print('The next number for the number',number, 'is', number+1)
 print('The previous number for the number',number, 'is', number-1)
