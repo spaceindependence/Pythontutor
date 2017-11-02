38c95bf4a3e67f5789ed4a31703b3a1b044d8b95
spaceindependence <spaceindependence@protonmail.com>
2017-11-02 22:25:16 +0300

    Can a king get from the first cell to the second one?

---
+++ ChessKing.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if (a == c + 1 or a == c - 1 or a == c) and ( b == d + 1 or b == d - 1 or b == d):
+    print('YES')
+else:
+    print('NO')
